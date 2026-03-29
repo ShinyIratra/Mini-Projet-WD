1aecac06d2c815ca85767153bdc5cf99927f2112
ShinyIratra <Iratra53@gmail.com>
2026-03-29 18:44:07 +0300

    fix : config_sample pour suivre postgres

--- app/config/config_sample.php
+++ app/config/config_sample.php
@@ -73,14 +73,10 @@ return [
 	 *         Database Settings          *
 	 **************************************/
 	'database' => [
-		// MySQL Example:
-		// 'host'     => 'localhost',      // Database host (e.g., 'localhost', 'db.example.com')
-		// 'dbname'   => 'your_db_name',   // Database name (e.g., 'flightphp')
-		// 'user'     => 'your_username',  // Database user (e.g., 'root')
-		// 'password' => 'your_password',  // Database password (never commit real passwords)
-
-		// SQLite Example:
-		// 'file_path' => __DIR__ . $ds . '..' . $ds . 'database.sqlite', // Path to SQLite file
+		'host'     => 'postgres',
+		'dbname'   => 'mp-wd-0326',
+		'user'     => 'postgres',
+		'password' => 'post',
 	],
 
 	/**************************************
