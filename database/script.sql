fadac951e7c7a4cb70114f54470f2144c81d4b10
RANDRIANASOLO Tafita Fitia <tafitaran05@gmail.com>
2026-03-29 17:50:36 +0300

    Add : script et schema database

---
+++ database/script.sql
@@ -0,0 +1,42 @@
+CREATE TABLE Utilisateur(
+   Id_Utilisateur SERIAL,
+   nom VARCHAR(100)  NOT NULL,
+   prenom VARCHAR(200)  NOT NULL,
+   identifiant VARCHAR(100)  NOT NULL,
+   password VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(Id_Utilisateur),
+   UNIQUE(identifiant)
+);
+
+CREATE TABLE Categorie(
+   Id_Categorie SERIAL,
+   rubrique VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(Id_Categorie)
+);
+
+CREATE TABLE Article(
+   Id_Article SERIAL,
+   titre TEXT NOT NULL,
+   contenu TEXT NOT NULL,
+   date_publication TIMESTAMP NOT NULL,
+   Id_Categorie INTEGER NOT NULL,
+   PRIMARY KEY(Id_Article),
+   FOREIGN KEY(Id_Categorie) REFERENCES Categorie(Id_Categorie)
+);
+
+CREATE TABLE Article_photo(
+   Id_Article_photo SERIAL,
+   chemin TEXT NOT NULL,
+   alt VARCHAR(255)  NOT NULL,
+   Id_Article INTEGER NOT NULL,
+   PRIMARY KEY(Id_Article_photo),
+   FOREIGN KEY(Id_Article) REFERENCES Article(Id_Article)
+);
+
+CREATE TABLE Auteur(
+   Id_Utilisateur INTEGER,
+   Id_Article INTEGER,
+   PRIMARY KEY(Id_Utilisateur, Id_Article),
+   FOREIGN KEY(Id_Utilisateur) REFERENCES Utilisateur(Id_Utilisateur),
+   FOREIGN KEY(Id_Article) REFERENCES Article(Id_Article)
+);
